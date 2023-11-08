c3018daf6d76610d951c3c1ffb782583d9b528d3
malteheimann <m.heimann@trenkwalder.com>
2023-11-09 00:09:55 +0100

    ionic branch added
    Signed-off-by: malteheimann <m.heimann@trenkwalder.com>

--- bin/dev-aws-cdk-graphql.ts
+++ bin/dev-aws-cdk-graphql.ts
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import {DevAwsCdkGraphqlStack} from '../lib/dev-aws-cdk-graphql-stack';
+import { DevAwsCdkGraphqlStack } from '../lib/dev-aws-cdk-graphql-stack';
 
+const frontendPath = './frontend/my-app/www';
 const domainName = 'trenkwalder-learning-mh.com';
-const hostedZoneId = 'Z1JY41Y266322D'
+const hostedZoneId = 'Z1JY41Y266322D';
 const certArnUsEast1 = 'arn:aws:acm:us-east-1:509130371655:certificate/ce789b81-c8ee-4da1-b80d-dd8cbb7133aa';
-
 
 const app = new cdk.App();
 
 new DevAwsCdkGraphqlStack(app, 'DevAwsCdkGraphqlStack', {
-    stageName: 'dev',
-    env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION,
-    },
-    domainName,
-    hostedZoneId,
-    certArnUsEast1
+	stageName: 'dev',
+	env: {
+		account: process.env.CDK_DEFAULT_ACCOUNT,
+		region: process.env.CDK_DEFAULT_REGION,
+	},
+	domainName,
+	hostedZoneId,
+	certArnUsEast1,
+	frontendPath,
 });
