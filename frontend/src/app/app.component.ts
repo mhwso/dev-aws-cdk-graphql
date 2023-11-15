0fc0f96a02f996d2a7af163c6cd9106455139e70
Malte Heimann <software@malte-heimann.de>
2023-11-15 15:00:29 +0100

    wip, but switching to ionic in new branch

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,12 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterOutlet} from '@angular/router';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'My frontend';
+  title = 'my-app'
+
+  onClick($event: MouseEvent) {}
 }
